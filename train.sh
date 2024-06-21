0b16fcffcf7f74aae57522d77117047dc64640d2
tridomanh <tridomanh123@gmail.com>
2024-06-21 15:37:46 +0700

    fix requirement and bash

--- train.sh
+++ train.sh
@@ -1,2 +1,4 @@
-# recommended paddle.__version__ == 2.0.0
-python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_mv3_none_bilstm_ctc.yml
+# train
+python3 -m paddle.distributed.launch --log_dir=./debug/ --gpus '0,1,2,3,4,5,6,7'  tools/train.py -c configs/rec/rec_multi_language_lite_train.yml 
+#infer
+python3 tools/infer_rec.py -c configs/rec/rec_multi_language_lite_train.yml -o Global.pretrained_model={path/to/weights}/best_accuracy Global.load_static_weights=false Global.infer_img={image path}
